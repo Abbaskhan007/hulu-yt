8a1b6d4d030b38f03f1f9974bbc18997943f49c1
Abbaskhan007 <nawababbas756@gmail.com>
2021-12-11 19:46:34 +0500

    Empty

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,43 @@
-import Head from "next/head"
-import Header from "../components/Header"
-import NavBar from "../components/NavBar"
-import Result from "../components/Result"
-import Axios from 'axios'
-import requests from "../utils/requests"
+// import Head from "next/head"
+// import Header from "../components/Header"
+// import NavBar from "../components/NavBar"
+// import Result from "../components/Result"
+// import Axios from 'axios'
+// import requests from "../utils/requests"
 
-export default function Home({data}) {
-  console.log('Data',data);
+// export default function Home({data}) {
+//   console.log('Data',data);
+//   return (
+//     <div>
+//       <Head>
+//         <title>Hulu</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+//       <Header />
+//       <NavBar/>
+//       <Result data={data}/>
+//     </div>
+//   )
+// }
+
+// export async function getServerSideProps(context){
+//   const genre = context.query.genre;
+    
+//   console.log('fetch',requests[genre])
+//   const data = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || genre?.fetchTrending?.url }`).then(res=>res.json())
+//   return {
+//       props: {
+//           data: data
+//       }
+//   }
+// }
+
+import React from 'react'
+
+export default function index() {
   return (
     <div>
-      <Head>
-        <title>Hulu</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <NavBar/>
-      <Result data={data}/>
+      <h1>Running next.js app</h1>
     </div>
   )
-}
-
-export async function getServerSideProps(context){
-  const genre = context.query.genre;
-    
-  console.log('fetch',requests[genre])
-  const data = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || genre?.fetchTrending?.url }`).then(res=>res.json())
-  return {
-      props: {
-          data: data
-      }
-  }
 }
