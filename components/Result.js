a8587da0cbca1b96beaad7660008b7fb41650e4c
Abbaskhan007 <nawababbas756@gmail.com>
2021-12-11 18:20:08 +0500

    Fixing erros before the deployment

--- components/Result.js
+++ components/Result.js
@@ -3,11 +3,10 @@ import Thumbnail from './Thumbnail'
 import { forwardRef } from 'react';
 
 const Result = forwardRef(({data},ref) =>  {
-    console.log('Results',data.results)
     return (
         <div ref={ref} className='sm:grid md:grid-cols-2 xl:grid-cols-3 px-5 my-10 3xl:flex flex-wrap justify-center'>
             {data.results.map(movieData=>(
-                <Thumbnail movieData={movieData}/>
+                <Thumbnail key={movieData.key} movieData={movieData}/>
             ))}
 
         </div>
