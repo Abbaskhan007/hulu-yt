8d122c5259d83cec0072639921fde0e6cb750486
Abbaskhan007 <nawababbas756@gmail.com>
2021-12-11 19:51:17 +0500

    Checking

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,33 @@
-// import Head from "next/head"
-// import Header from "../components/Header"
-// import NavBar from "../components/NavBar"
-// import Result from "../components/Result"
-// import Axios from 'axios'
-// import requests from "../utils/requests"
+import Head from "next/head"
+import Header from "../components/Header"
+import NavBar from "../components/NavBar"
+import Result from "../components/Result"
+import Axios from 'axios'
+import requests from "../utils/requests"
 
-// export default function Home({data}) {
-//   console.log('Data',data);
-//   return (
-//     <div>
-//       <Head>
-//         <title>Hulu</title>
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
-//       <Header />
-//       <NavBar/>
-//       <Result data={data}/>
-//     </div>
-//   )
-// }
-
-// export async function getServerSideProps(context){
-//   const genre = context.query.genre;
-    
-//   console.log('fetch',requests[genre])
-//   const data = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || genre?.fetchTrending?.url }`).then(res=>res.json())
-//   return {
-//       props: {
-//           data: data
-//       }
-//   }
-// }
-
-import React from 'react'
-
-export default function index() {
+export default function Home({data}) {
+  console.log('Data',data);
   return (
     <div>
-      <h1>Running next.js app</h1>
+      <Head>
+        <title>Hulu</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <NavBar/>
+      <Result data={data}/>
     </div>
   )
+}
+
+export async function getServerSideProps(context){
+  const genre = context.query.genre;
+    
+  console.log('fetch',requests[genre])
+  const data = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || genre?.fetchTrending?.url }`).then(res=>res.json())
+  return {
+      props: {
+          data: data
+      }
+  }
 }
